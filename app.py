3d50f2053bb207467743e7808c085fbaf3f65f8b
Ilano Wouters <ilano.wouters@student.ucll.be>
2025-12-16 14:20:35 +0100

    menu page

--- app.py
+++ app.py
@@ -4,11 +4,32 @@ def create_main_surface():
     screen_size = (1024, 786)
     return pygame.display.set_mode(screen_size)
 
+def clear_surface(surface):
+    surface.fill((0, 0, 0))
+
+def render_frame(surface, x):
+    pygame.draw.circle(
+        surface, (255, 0, 0), (x, 393), 50
+    )
+    pygame.display.flip()
+
 def main():
     pygame.init()
-    screen = create_main_surface()
+    surface = create_main_surface()
 
-    while True:
-        pass
+    x = 0
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                print(f"A key was pressed! Key code: {event.key}")
+
+        render_frame(surface, x)
+        x += 1
+
+    pygame.quit()
 
 main()
